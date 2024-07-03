6f3c365707adfba7574b1cef48f13d9214048b8c
Lailadej <163850967+Lailadej@users.noreply.github.com>
2024-07-02 21:34:47 -0400

    Creacion de UI Y SCRIPT
    Como hacer para que los objetos aparescan bien en la esena. Como poner precio en cada objeto

---
+++ Assets/ArrayProductos.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrayProductos : MonoBehaviour
+{
+    public GameObject[] ArrayProducto;
+
+              
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
